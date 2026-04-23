418226b6cd3345d6481040bb3bce496aadc74837
RAFAEL GRACIA <djrafael1004@gmail.com>
2026-04-23 17:26:55 -0500

    feat: redirect home page to login

--- src/app/(app)/page.jsx
+++ src/app/(app)/page.jsx
@@ -1,5 +1,5 @@
-import { DashboardView } from '@/components/DashboardView'
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return <DashboardView />
+  redirect("/login");
 }
