afbdf6c5d745b28d0c701cfbaf393714128c3051
rafael <djrafael1004@gmail.com>
2026-06-10 15:46:58 -0500

    mejroa

--- src/app/(app)/prestamos/page.jsx
+++ src/app/(app)/prestamos/page.jsx
@@ -1,5 +1,12 @@
+'use client'
+
+import { Suspense } from 'react'
 import { PrestamosView } from '@/components/PrestamosView'
 
 export default function PrestamosPage() {
-  return <PrestamosView />
+  return (
+    <Suspense fallback={<div style={{ padding: '2rem', textAlign: 'center' }}>Cargando préstamos...</div>}>
+      <PrestamosView />
+    </Suspense>
+  )
 }
